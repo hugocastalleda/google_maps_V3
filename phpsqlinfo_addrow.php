db2c42bfea371cc747ddf10a7ec7ce506d2e33d4
hugo castañeda <hugocastalleda@gmail.com>
2013-04-04 12:43:55 -0500

    nuevos cambios

--- phpsqlinfo_addrow.php
+++ phpsqlinfo_addrow.php
@@ -2,15 +2,7 @@
 require("phpsqlinfo_dbinfo.php");
 
 // Gets data from URL parameters
-$pais = $_GET['pais'];
-$departamento = $_GET['departamento'];
-$municipio = $_GET['municipio'];
-$barrio = $_GET['barrio'];
-$direccion = $_GET['direccion'];
-$type = $_GET['type'];
-$img = $_GET['img'];
-$lat = $_GET['lat'];
-$lng = $_GET['lng'];
+
 
 
 // Opens a connection to a MySQL server
@@ -25,24 +17,70 @@ if (!$db_selected) {
   die ('Can\'t use db : ' . mysql_error());
 }
 
+// We're putting all our files in a directory called images.
+$uploaddir = 'images/';
+
+// The posted data, for reference
+$pais = $_POST['pais'];
+$departamento = $_POST['departamento'];
+$municipio = $_POST['municipio'];
+$barrio = $_POST['barrio'];
+$direccion = $_POST['direccion'];
+$type = $_POST['type'];
+$lat = $_POST['lat'];
+$lng = $_POST['lng'];
+$file = $_POST['value'];
+$name = $_POST['name'];
+
+// Get the mime
+$getMime = explode('.', $name);
+$mime = end($getMime);
+
+// Separate out the data
+$data = explode(',', $file);
+
+// Encode it correctly
+$encodedData = str_replace(' ','+',$data[1]);
+$decodedData = base64_decode($encodedData);
+
+// You can use the name given, or create a random name.
+// We will create a random name!
+
+$randomName = substr_replace(sha1(microtime(true)), '', 12).'.'.$mime;
+
+
+if(file_put_contents($uploaddir.$randomName, $decodedData)) {
+
+
 // Insert new row with user data
 $query = sprintf("INSERT INTO markers " .
          " (id, pais, departamento, municipio, barrio, direccion, type, img, lat, lng) " .
          " VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
-         mysql_real_escape_string($pais),
-         mysql_real_escape_string($departamento),
-		 mysql_real_escape_string($municipio),
-		 mysql_real_escape_string($barrio),
-		 mysql_real_escape_string($direccion),
-		 mysql_real_escape_string($type),
-		 mysql_real_escape_string($img),
-         mysql_real_escape_string($lat),
-         mysql_real_escape_string($lng));
+         mysql_real_escape_string(utf8_decode($pais)),
+         mysql_real_escape_string(utf8_decode($departamento)),
+		 mysql_real_escape_string(utf8_decode($municipio)),
+		 mysql_real_escape_string(utf8_decode($barrio)),
+		 mysql_real_escape_string(utf8_decode($direccion)),
+		 mysql_real_escape_string(utf8_decode($type)),
+		 mysql_real_escape_string(utf8_decode($randomName)),
+         mysql_real_escape_string(utf8_decode($lat)),
+         mysql_real_escape_string(utf8_decode($lng)));
 
 $result = mysql_query($query);
 
 if (!$result) {
   die('Invalid query: ' . mysql_error());
 }
+
+
+
+
+	echo $randomName.":uploaded successfully";
+}
+else {
+	// Show an error message should something go wrong.
+	echo "Something went wrong. Check that the file isn't corrupted";
+}
+
 
 ?>
